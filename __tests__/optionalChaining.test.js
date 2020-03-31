73054196e832a572301775c1e413a42516603715
二货机器人 <smith3816@gmail.com>
2020-03-31 17:10:07 +0800

    test: Add optional chaining test (#9)
    * add test case
    
    * add optional chaining test case

---
+++ __tests__/optionalChaining.test.js
@@ -0,0 +1,16 @@
+const ts2js = require('../lib/ts2js').default;
+
+function parse(text, option) {
+  const parsed = ts2js([{ data: text.trim() }], option)[0].data;
+  return parsed;
+}
+
+describe('optional chaining', () => {
+  it('class', () => {
+    const source = `
+const getRowByKey = (key: string, newData?: TableFormDateType[]) => (newData || data)?.filter((item) => item.key === key)[0];
+`.trim();
+    const text = parse(source);
+    console.log('Output:', text);
+  });
+});
